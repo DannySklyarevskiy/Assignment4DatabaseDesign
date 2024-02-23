1cbd7185e47de6e69471ef7bce007633926ac266
DannySklyarevskiy <137563220+DannySklyarevskiy@users.noreply.github.com>
2024-02-22 18:21:51 -0800

    finalize creative addition

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- 1) (5 pts) What are the last names and emails of all customer who made purchased in the store?
-SELECT LastName FROM customers;
+SELECT LastName, Email FROM customers;
 
 -- 2) (5 pts) What are the names of each album and the artist who created it?
 SELECT albums.Title AS "Album Title", artists.Name AS "Artist Name"
@@ -31,7 +31,10 @@ FROM albums
 INNER JOIN artists ON albums.ArtistId = artists.ArtistId
 INNER JOIN tracks ON albums.AlbumId = tracks.AlbumId
 INNER JOIN media_types ON tracks.MediaTypeId = media_types.MediaTypeId
-WHERE media_types.Name LIKE '%MPEG%';
+INNER JOIN playlist_track ON playlist_track.TrackId = tracks.TrackId
+INNER JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId
+WHERE media_types.Name LIKE '%MPEG%'
+AND(playlists.Name = "Brazilian Music" OR playlists.Name = "Grunge");
 
 -- 7) (20 pts) How many artists published at least 10 MPEG tracks?
 SELECT count(artistName) 
@@ -44,7 +47,7 @@ FROM albums
 INNER JOIN artists ON albums.ArtistId = artists.ArtistId
 INNER JOIN tracks ON albums.AlbumId = tracks.AlbumId
 INNER JOIN media_types ON tracks.MediaTypeId = media_types.MediaTypeId
-WHERE media_types.Name LIKE '%MPEG%' AND artists.Name=artistName) > 10);
+WHERE media_types.Name LIKE '%MPEG%' AND artists.Name=artistName) >= 10);
 
 -- 8) (25 pts) What is the total length of each playlist in hours? 
 -- I used left join to get all playlists, even those with null values
@@ -65,3 +68,19 @@ HAVING hours > 2
 
 -- 9) (25 pts) Creative addition: Define a new meaningful query using at least three tables, 
 --and some window function. Explain clearly what your query achieves, and what the results mean
+-- My query: Get the names and track amounts of all playlists with an above average amount of tracks, 
+-- excluding playlists with no tracks
+SELECT playlists.Name, COUNT(tracks.TrackId) AS TrackCount
+FROM playlists
+LEFT JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
+LEFT JOIN tracks ON playlist_track.TrackId = tracks.TrackId
+GROUP BY playlists.Name
+HAVING TrackCount > (
+    SELECT AVG(TrackCount) 
+    FROM (
+        SELECT COUNT(TrackId) AS TrackCount
+        FROM playlist_track
+        GROUP BY PlaylistId
+    )
+);
+-- The query returns only two playlists, as those made the average amount of tracks per playlist extremely high
